d08f9a5e9bfaf0d2f090a366343de6e5b8c6370b
Benjamin Calderon <benj.calderon@gmail.com>
2018-11-14 17:47:20 -0500

    WIP dob selector

--- src/components/ContactInfo.vue
+++ src/components/ContactInfo.vue
@@ -4,15 +4,28 @@
             <svg :data-jdenticon-value="firstName + lastName" width="80" height="80" />
         </div>
 
-        <span class="pure-form" v-if="editing">
-            <input type="text" v-model="firstName" size="10">
-            <input type="text" v-model="lastName" size="10">
-            <button class="button-success pure-button">✓</button>
-            <button class="button-error pure-button">x</button>
-        </span>
-        <hash-icon />
-        <h1 v-if="!editing" @click="editing = !editing">{{firstName}} {{lastName}}</h1>
+        <div class="pure-form" v-if="editing">
+            <legend>Full Name</legend>
+            <span class="pure-group">
+                <input id="f" type="text" v-model="firstName">
+                <input id="l" type="text" v-model="lastName">
+            </span>
 
+            <legend>Date of Birth</legend>
+                <v-date-picker
+                    v-model='dateOfBirth'>
+                </v-date-picker>
+            
+            <button class="button-success pure-button">✓</button>
+            <button class="button-error pure-button" @click="editing = !editing">x</button>
+            
+            
+            
+        </div>
+        <hash-icon />
+        <h1 v-if="!editing">{{firstName}} {{lastName}}</h1>
+        
+        <button class="pure-button" @click="editing = !editing" v-if="!editing">Edit</button>
     </div>
 </template>
 
@@ -21,8 +34,9 @@ export default {
     data() {
         return {
             editing: false,
-            firstName: 'Full',
-            lastName: 'Name'
+            firstName: 'Albert',
+            lastName: 'Einstein',
+            dateOfBirth: '',
         }
     }
 }
@@ -35,22 +49,18 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: center;
-
-    span.pure-form {
-        display: flex;
-    }
 }
 
 .button-error {
-            background: rgb(202, 60, 60);
-            color: #FFF;
-            font-weight: 700;
-        }
-        .button-success {
-            background: rgb(28, 184, 65);
-            color: #FFF;
-            font-weight: 700;
-        }
-
-
+    background: rgb(202, 60, 60);
+    color: #FFF;
+    font-weight: 700;
+    height: 38px;
+}
+.button-success {
+    background: rgb(28, 184, 65);
+    color: #FFF;
+    font-weight: 700;
+    height: 38px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import 'purecss'
+import VCalendar from 'v-calendar';
+import 'v-calendar/lib/v-calendar.min.css';
 
+Vue.use(VCalendar);
 Vue.config.productionTip = false
 
 new Vue({
